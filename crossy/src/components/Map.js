205ec9a2dd5ea72816d5c6bff997956e6caaf9c0
beastcode <beastcode9999@gmail.com>
2025-08-22 17:43:05 +0930

    add metadata and addRow function

--- crossy/src/components/Map.js
+++ crossy/src/components/Map.js
@@ -8,7 +8,8 @@
 // lets you move, rotate, or scel the entire map as a single object
 
 import * as THREE from "three"
-import { Grass } from "./Grass"
+import { Grass } from "./Grass" // grass function that screst the grass rows
+import { Tree } from "./Tree" // tree function that creates the tree objects
 
 
 /**
@@ -33,6 +34,30 @@ export const metadata = [
 export const map = new THREE.Group() // container for 3D objects
 
 export function initialiseMap() {
+    // creates first row
     const grass = Grass(0)
     map.add(grass)
+    addRows() // generate additional rows from metadata
+}
+
+// Generates 3D objects based on the metadata and adds them to the map container
+// loops through each row in metadata
+export function addRows() {
+    metadata.forEach((rowData, index) => {
+        const rowIndex = index + 1; // starts after row 0 aka (initial grass row)
+
+        if (rowData.type === "forest") { // if row is forest
+            // create a gradd with the given row index
+            const row = Grass(rowIndex)
+            // loop through trees array
+            // create the tree with the given tile index and height
+            // add tree to the row group
+            rowData.trees.forEach(({ tileIndex, height }) => {
+                const three = Tree(tileIndex, height)
+                row.add(three)
+            })
+            // add the completed row to the map group
+            map.add(row)
+        }
+    })
 }
