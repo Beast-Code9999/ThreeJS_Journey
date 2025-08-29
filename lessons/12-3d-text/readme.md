a949073c56d6c832597ea3b739be103a5174ab1e
Beast Code <beastcode9999@gmail.com>
2025-08-29 16:53:15 +0930

    finish text lesson

--- lessons/12-3d-text/readme.md
+++ lessons/12-3d-text/readme.md
@@ -14,3 +14,6 @@ npm run dev
 # Build for production in the dist/ directory
 npm run build
 ```
+
+# Useful links
+https://gero3.github.io/facetype.js/
--- lessons/12-3d-text/src/script.js
+++ lessons/12-3d-text/src/script.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 /**
  * Base
@@ -15,20 +16,87 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Axes helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-
+const matcapTexture = textureLoader.load(
+    '/textures/matcaps/8.png'
+)
+matcapTexture.colorSpace = THREE.SRGBColorSpace
 /**
- * Object
+ * Fonts
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        const textGeometry = new TextGeometry(
+            'Hello Three.js',
+            {
+                font: font,
+                size: 0.5,
+                depth: 0.2,
+                curveSegments: 6,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                // think of bevelSize like 3d borders
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        // check the box geometry 
+        // textGeometry.computeBoundingBox()
+
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
+        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
+        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
+        // )
+        
+        textGeometry.center()
+        
+        const textMaterial = new THREE.MeshMatcapMaterial({
+            // wireframe: true
+            matcap: matcapTexture
+        })
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
+
+        // define geometry first
+        // define material
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+        // or you can just define one material, instead of textmaterial and donutMaterial, just a material
+
+        for(let i = 0; i < 100; i++)
+        {
+            // then define mesh, 100 of them
+            const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+            scene.add(donut)
+
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+        }
+    }
 )
 
-scene.add(cube)
+
 
 /**
  * Sizes
